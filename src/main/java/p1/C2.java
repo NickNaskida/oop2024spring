fe9ed450e2b14cad694ebc4ecf3a9407c27431ea
Nikoloz Naskidashvili <nikoloz2005@gmail.com>
2024-03-18 10:32:34 +0400

    feat: add lecture 2 material

---
+++ src/main/java/p1/C2.java
@@ -0,0 +1,10 @@
+package p1;
+
+public class C2 {
+    public static final String MESSAGE = "This is C2 message";
+
+    public static void main(String[] args) {
+        System.out.println(C1.MESSAGE);
+    }
+}
+
